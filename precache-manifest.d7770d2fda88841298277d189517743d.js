1f4fe64e4f85de14d380fe28827194b9cffbd737
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-06-01 16:01:33 +0100

    Updates

---
+++ precache-manifest.d7770d2fda88841298277d189517743d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff921b8cff549b48865affe911e087b",
+    "url": "/Australia/index.html"
+  },
+  {
+    "revision": "3046f54d513d9c653c43",
+    "url": "/Australia/static/css/main.01e1297a.chunk.css"
+  },
+  {
+    "revision": "1bed36e4513d1d186ad9",
+    "url": "/Australia/static/js/2.3cd06ff2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Australia/static/js/2.3cd06ff2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3046f54d513d9c653c43",
+    "url": "/Australia/static/js/main.8e9fd774.chunk.js"
+  },
+  {
+    "revision": "ba2d5e5c24f7a4d816a0",
+    "url": "/Australia/static/js/runtime-main.b562677d.js"
+  },
+  {
+    "revision": "381c3e059583696db7503805343f6c26",
+    "url": "/Australia/static/media/Bondi.381c3e05.jpg"
+  },
+  {
+    "revision": "055ac899751450a298e422496d97f091",
+    "url": "/Australia/static/media/Linkedin.055ac899.svg"
+  },
+  {
+    "revision": "18912f4485cf4955136ecb581f16b799",
+    "url": "/Australia/static/media/Melbourne.18912f44.jpg"
+  },
+  {
+    "revision": "89ec7fd9d4bb44b1a3d7a9191a6a3dc4",
+    "url": "/Australia/static/media/arrow.89ec7fd9.svg"
+  },
+  {
+    "revision": "27c9686c896cb43e19e6de18aa07cc5b",
+    "url": "/Australia/static/media/australia.27c9686c.png"
+  },
+  {
+    "revision": "3d39cebc0c5dd23ef8b8efe7a5350bb5",
+    "url": "/Australia/static/media/australiaMap.3d39cebc.jpg"
+  },
+  {
+    "revision": "246309cecc4230165a69c9280a5c05df",
+    "url": "/Australia/static/media/brisbane.246309ce.jpg"
+  },
+  {
+    "revision": "9863df11ae48ecee1a9c20a3cf2f9c33",
+    "url": "/Australia/static/media/globe.9863df11.svg"
+  },
+  {
+    "revision": "74391b2580da07d412c876d3994ee70f",
+    "url": "/Australia/static/media/instagram.74391b25.svg"
+  },
+  {
+    "revision": "ba9323ea6b8e924c93a36cf7152b93d1",
+    "url": "/Australia/static/media/kangaroo.ba9323ea.jpg"
+  },
+  {
+    "revision": "07ec53790bc0b861a49c5c0d4e2f2732",
+    "url": "/Australia/static/media/search.07ec5379.svg"
+  },
+  {
+    "revision": "4020de0b53c4cacb5728def0f5bb2058",
+    "url": "/Australia/static/media/sydney.4020de0b.jpg"
+  },
+  {
+    "revision": "0b64fb7fa4bb2c192e99c63f86cb54f0",
+    "url": "/Australia/static/media/twitter.0b64fb7f.svg"
+  }
+]);
